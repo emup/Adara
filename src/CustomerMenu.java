a7c9152ba9f555c0510d61f4cd8711a70a1247de
emup <emuppet@protonmail.com>
2022-06-14 17:18:16 +0200

    Moved menu's and their methods for SRP and to solve long object in Menu.java

---
+++ src/CustomerMenu.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CustomerMenu extends Menu {
+    private static final ArrayList<String> customerMenu = new ArrayList<>(Arrays.asList(
+            ANSI_CYAN + "Klanten:" + ANSI_RESET,
+            "1: Voeg klant toe",
+            "2: Toon klanten",
+            "0: Terug\n"
+    ));
+
+    public static void showCustomerMenu(){
+        while(checkSelected()) {
+            for(String c : customerMenu) {
+                System.out.println(c);
+            }
+
+            selected = scanner.nextInt();
+
+            switch(selected){
+                case 1: Customer.addCustomer();
+                case 2: Customer.showCustomers();
+                case 0:
+                    selected = null;
+                    mainMenu();
+                default:
+                    System.out.println("Ongeldige input! Voer een geldig input in: ");
+            }
+        }
+    }
+
+}
---
+++ src/OrderMenu.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OrderMenu extends Menu {
+    private static final ArrayList<String> orderMenu = new ArrayList<>(Arrays.asList(
+            Main.ANSI_CYAN + "Bestellingen:" + Main.ANSI_RESET,
+            "1: Voeg bestelling toe",
+            "2: Voeg kisten toe aan bestelling",
+            "3: Toon bestellingen",
+            "4: Toon volledig vervulde bestellingen",
+            "0: Terug\n"
+    ));
+
+    public static void showOrderMenu(){
+        while(checkSelected()) {
+            for(String o : orderMenu) {
+                System.out.println(o);
+            }
+
+            selected = scanner.nextInt();
+
+            switch(selected){
+                case 1: Order.addOrder();
+                case 2: Order.fulfillOrder();
+                case 3: Order.showOrders();
+                case 4: Order.showOrdersFulfilled();
+                case 0:
+                    selected = null;
+                    mainMenu();
+                default:
+                    System.out.println("Ongeldige input! Voer een geldig input in: ");
+            }
+        }
+    }
+
+}
